eebdbafc080ac64afd467d124619420cf5337eec
Vladislav Podkaura <tripledoublehs@ukr.net>
2019-12-02 00:47:58 +0200

    Updates

---
+++ precache-manifest.76a3b13458c1c9a3cd4bca0618bc5989.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e012a7fafdc8d4ad5aa69c7a13d1fc93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8077380e99b6d5432094",
+    "url": "/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "621e795f2d79a66596ca",
+    "url": "/static/js/2.6fe8b666.chunk.js"
+  },
+  {
+    "revision": "8077380e99b6d5432094",
+    "url": "/static/js/main.8c058884.chunk.js"
+  },
+  {
+    "revision": "2c125b872f9c0b3d49c9",
+    "url": "/static/js/runtime-main.e5a83183.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
